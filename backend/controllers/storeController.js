124702b50527c289f074687debe2d95626d8a146
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-09-16 02:57:53 -0300

    Created router for add store item

--- backend/controllers/storeController.js
+++ backend/controllers/storeController.js
@@ -24,4 +24,25 @@ router.post('/', async (req, res) => {
 
 });
 
+router.post('/:id/items', async (req, res) => {
+
+  let item = req.body;
+
+  Store.findById(req.params.id, function (err, store) {
+    if (err) return res.status(400).send({ error: "store not registered" });
+
+    if (!store.storage) {
+      store.storage = []
+    }
+
+    store.storage.push(item);
+
+    store.save(function (err, updatedStore) {
+      if (err) return res.status(400).send({ error: 'Registration failed: ' + err });
+
+      return res.send(updatedStore);
+    });
+  });
+});
+
 module.exports = app => app.use("/stores", router);
