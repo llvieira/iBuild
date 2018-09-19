6bd17c03f4644e5f15ecba1776b9649403688d70
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-09-18 23:55:39 -0300

    refactored open routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,58 +1,28 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
 const User = require("../models/user");
-const authConfig = require("../config/auth");
 const util = require("../util/util");
 
 const router = express.Router();
 
-function generateToken( params = {}){
-    return jwt.sign(params, authConfig.secret, {
-        expiresIn: 86400
-    });
-}
-
-router.post('/register', async (req, res) => {
-    const {email} = req.body;
-
-    try{
-
-        if ( await User.findOne({ email })){
-            return res.status(400).send({ error: "Usuário já cadastrado" });
-        }
-
-        const user = await User.create(req.body);
-
-        user.password = undefined;
-
-        util.sendEmail(email);
-        return res.send({user, token: generateToken({id: user.id})});
-    } catch (e) {
-        console.log(e.message);
-        return res.status(400).send({error: 'Registration failed'});
-    }
-});
-
-router.post('/authenticate', async (req, res) => {
-    const {email, password} = req.body;
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password');
 
-    if (!user){
-        return res.status(400).send({ error: 'User not found'});
+    if (!user) {
+        return res.status(400).send({ error: 'User not found' });
     }
 
-    if (! await bcrypt.compare(password, user.password)){
-        return res.status(400).send({error: 'Invalid password' });
+    if (! await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ error: 'Invalid password' });
     }
 
     user.password = undefined;
 
     res.send({
         user,
-        token: generateToken({ id: user.id})
+        token: util.generateToken({ id: user.id })
     });
 });
 
--- backend/middlewares/open.js
+++ backend/middlewares/open.js
@@ -1,4 +1,5 @@
 
+//TO-DO: Add other open routes
 module.exports = (req, res, next) => {
   if (req.path === '/' && req.method === 'POST') {
     next('router');
