04b2ed558a00cf4a454f055aae9e9fe771751c90
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-09-23 19:04:23 -0300

    Finalize layout

--- frontend/ibuild/src/layout/RegisterLayout.js
+++ frontend/ibuild/src/layout/RegisterLayout.js
@@ -1,9 +1,8 @@
 import React from "react";
 import RegisterUserLayout from './RegisterUserLayout';
 
-const RegisterLayout = ({children}) => (
-    <div className="jumbotron" style={{textAlign: "center", width: "50%", margin: "2% auto"}}>
-        <h1 style={{fontWeight: "bold"}}>iBuild</h1>
+const RegisterLayout = () => (
+    <div className="register-layout">
         <RegisterUserLayout></RegisterUserLayout>
     </div>
 );
--- frontend/ibuild/src/layout/RegisterUserLayout.js
+++ frontend/ibuild/src/layout/RegisterUserLayout.js
@@ -2,20 +2,29 @@ import React from "react";
 import Input from "../components/input/Input";
 import AcceptButton from "../components/button/AcceptButton";
 import request from "../config";
+import { history } from '../config/history'
+import './layout.css';
 
 export default class RegisterUserLayout extends React.Component {
     constructor() {
         super();
         this.state = {
-            user: {}
+            user: {
+                name: '',
+                email: '',
+                password: ''
+            }
         }
     }
 
     changeProperty(porpertyName) {
         return event => {
             const value = event.target.value;
-            this.state.user[porpertyName] = value;
-            console.log(this.state.user);
+            const user = {
+                ...this.state.user
+            };
+            user[porpertyName] = value;
+            this.setState({user});
         }
     }
 
@@ -26,7 +35,7 @@ export default class RegisterUserLayout extends React.Component {
         request(path, method, this.state.user, {
             "Content-Type": "application/json"
         }).then(response => {
-            window.location.replace('/success')
+            history.push('/success');
         });
 
         return false;
@@ -41,12 +50,19 @@ export default class RegisterUserLayout extends React.Component {
         ];
 
         return (
-            <div>
-                <h4>Cadastro de Usuário</h4>
-                <form name="registerUser" onSubmit={this.registerUser.bind(this)}>
-                    {inputs}
-                    <AcceptButton></AcceptButton>
-                </form>
+            <div className="shadow">
+                <div className="background-top">
+                    <h4 style={{margin: 0}}>Criar conta</h4>
+                </div>
+                <div className="background-bottom">
+                    <form name="registerUser" onSubmit={this.registerUser.bind(this)}>
+                        {inputs}
+                        <div className="container">
+                            <p>Eu aceito os <a href="https://google.com.br">termos de uso</a></p>
+                            <AcceptButton></AcceptButton>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
