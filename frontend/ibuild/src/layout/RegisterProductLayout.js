09e9dda1b4587a81ff4767f978aecf7b7ea92f46
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-11-08 22:29:01 -0300

    Add warning alert for registerProduct

--- frontend/ibuild/src/layout/RegisterProductLayout.js
+++ frontend/ibuild/src/layout/RegisterProductLayout.js
@@ -11,21 +11,24 @@ class RegisterProductLayout extends Component {
         img: "",
         title: "",
         value: 0,
-        delivery: false,
+        delivery: "false",
         brand: "",
         category: "",
         quantity: 0
+      },
+      show: false,
+      alert: {
+        text: ''
       }
     };
   }
 
-  changeProperty(porpertyName) {
+  changeRegisterProperty(porpertyName) {
     return event => {
       const value = event.target.value;
       const product = {
         ...this.state.product
       };
-
 
       product[porpertyName] = value;
       this.setState({ product });
@@ -34,19 +37,24 @@ class RegisterProductLayout extends Component {
 
   registerProduct(e) {
     e.preventDefault();
-    const store = JSON.parse(localStorage.getItem('store'));
     const method = 'POST';
-    const path = '/stores/' + store._id + '/items';
+    const path = '/stores/items';
     request(path, method, this.state.product, {
       "Content-Type": "application/json",
       "Authorization": "Bearer " + localStorage.getItem('storeToken')
     }).then(response => {
       if (response.ok) {
-        history.push('/');
-        console.log('Success');
+        response.json().then(data => {
+          console.log(data);
+          history.push('/');
+        });
       } else {
-        console.log('Error!');
+        response.json().then(data => {
+          this.setState({ alert: { show: true, text: data.error } });
+        });
       }
+    }).catch(error => {
+      this.setState({ alert: { show: true, text: error.toString() } });
     });
   }
 
@@ -65,24 +73,29 @@ class RegisterProductLayout extends Component {
               </div>
               <div className="span8">
                 <h4 className="title"><span className="text"><strong>Cadastrar</strong> novo produto</span></h4>
+                <div className="span5">
+                  <div className="alert alert-warning" style={this.state.alert.show ? undefined : { display: 'none' }}>
+                    {this.state.alert.text}
+                  </div>
+                </div>
                 <form className="form-stacked" onSubmit={this.registerProduct.bind(this)} >
                   <fieldset>
                     <div className="control-group">
                       <label className="control-label">Nome:</label>
                       <div className="controls">
-                        <input type="text" placeholder="Coloque o nome do produto" className="input-xlarge" value={this.state.product.title} onChange={this.changeProperty("title").bind(this)} />
+                        <input type="text" placeholder="Coloque o nome do produto" className="input-xlarge" value={this.state.product.title} onChange={this.changeRegisterProperty("title").bind(this)} />
                       </div>
                     </div>
                     <div className="control-group">
                       <label className="control-label">Marca:</label>
                       <div className="controls">
-                        <input type="text" placeholder="Coloque a marca do produto" className="input-xlarge" value={this.state.product.brand} onChange={this.changeProperty("brand").bind(this)} />
+                        <input type="text" placeholder="Coloque a marca do produto" className="input-xlarge" value={this.state.product.brand} onChange={this.changeRegisterProperty("brand").bind(this)} />
                       </div>
                     </div>
                     <div className="control-group">
                       <label className="control-label">Categoria:</label>
                       <div className="controls">
-                        <select className="span3" value={this.state.product.category} onChange={this.changeProperty("category").bind(this)}>
+                        <select className="span3" value={this.state.product.category} onChange={this.changeRegisterProperty("category").bind(this)}>
                           <option>Ferramentas</option>
                           <option>Construção</option>
                           <option>Madeiras</option>
@@ -92,19 +105,26 @@ class RegisterProductLayout extends Component {
                     <div className="control-group">
                       <label className="control-label">Preço:</label>
                       <div className="controls">
-                        <input type="number" placeholder="Preço do produto" className="input-large" step="0.01" value={this.state.product.value} onChange={this.changeProperty("value").bind(this)} />
+                        <input type="number" placeholder="Preço do produto" className="input-large" step="0.01" value={this.state.product.value} onChange={this.changeRegisterProperty("value").bind(this)} />
                       </div>
                     </div>
                     <div className="control-group">
                       <label className="control-label">Quantidade:</label>
                       <div className="controls">
-                        <input type="number" placeholder="Quantidade do produto" className="input-large" value={this.state.product.quantity} onChange={this.changeProperty("quantity").bind(this)} />
+                        <input type="number" placeholder="Quantidade do produto" className="input-large" value={this.state.product.quantity} onChange={this.changeRegisterProperty("quantity").bind(this)} />
+                      </div>
+                    </div>
+                    <div className="control-group">
+                      <label className="control-label">Entrega em casa:</label>
+                      <div className="controls controls-row">
+                        <label className="radio span1"><input type="radio" value="true" checked={this.state.product.delivery === "true"} onChange={this.changeRegisterProperty("delivery").bind(this)} />Sim</label>
+                        <label className="radio span1"><input type="radio" value="false" checked={this.state.product.delivery === "false"} onChange={this.changeRegisterProperty("delivery").bind(this)} />Não</label>
                       </div>
                     </div>
                     <div className="control-group">
                       <label className="control-label">Foto (URL foto):</label>
                       <div className="controls">
-                        <input type="url" placeholder="Url da foto" className="input-large" value={this.state.product.img} onChange={this.changeProperty("img").bind(this)} />
+                        <input type="url" placeholder="Url da foto" className="input-large" value={this.state.product.img} onChange={this.changeRegisterProperty("img").bind(this)} />
                       </div>
                     </div>
                     <div className="control-group controls-row">
